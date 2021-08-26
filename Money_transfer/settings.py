cf8b817f249d0a953ca7b0b3ac79e02206177ec0
PaulGrishunin <paul_gri@mail.ru>
2021-08-26 02:11:44 +0200

    make it better

--- Money_transfer/settings.py
+++ Money_transfer/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -131,3 +133,8 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+db_from_env = dj_database_url.config()
+# DATABASE['default'].update(db_from_env)
+
+django_heroku.settings(locals())
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn Money_transfer.wsgi --log-file -
